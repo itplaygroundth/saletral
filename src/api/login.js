4ef2e6d3ab7c0842356148ccbd35f2a1ca4d18be
SHEEPTOYS\ACER NITRO 5 <itplayground@hotmail.com>
2018-08-25 15:10:26 +0700

    edit login.js

--- src/api/login.js
+++ src/api/login.js
@@ -56,7 +56,7 @@ export default {
     };
     // console.log(JSON.stringify({ username: creds.user, password: creds.password }));
     //  console.log(LOGIN_URL);
-    
+  
     return Axios.get(LOGIN_URL, {
       params: {
         username: creds.user, 
